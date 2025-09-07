ede8b3288ae813ee9af5c414db7d357d9ee241fd
EunhoJ <john.vieira99@gmail.com>
2025-09-07 13:04:54 -0300

    Repository e Comp SpinLoader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import SpinLoader from "@/components/SpinLoader";
+
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-xl font-bold text-blue-500 hover:text-blue-50 hover:bg-blue-500 transition duration-150">Text no meu H1</h1>
+      <SpinLoader/>
     </div>
   )
 }
